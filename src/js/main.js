824df646b773fae01c8be6ae005d47e2a7a02af7
Unknown <uma9266@gmail.com>
2018-04-03 03:16:18 +0900

    とりあえずレンダリングまで

--- src/js/main.js
+++ src/js/main.js
@@ -1,79 +1,21 @@
 'use strict'
 
 const DEBUG_MODE = false;
-import AudioManager from "./AudioManager";
 
 // const css = require('../styl/main.styl');
 
 
 
-import '../styl/controller.less';
-const controller = require('../pug/JintaiViewerController.pug');
-const volumedom = require('../pug/audioButton.pug');
-const debugviewr = require('../pug/debugViewr.pug');
-const messageimgs = require('../pug/messages.pug');
-import SceneManager from './SceneManager';
-import ScenePartcles from './ScenePartcles';
-import ControllerStyle from "./ControllerStyle";
-import InteractiveBox from "./InteractiveBox";
-import GUI from "./gui";
-
+import '../styl/main.less';
+import RendererStudy from './rendererStudy';
+//
 
 
 const MainStart =  ()=> {
 
+    const scene = new RendererStudy();
 
-
-    if(DEBUG_MODE)
-    {
-        var debug = document.createElement('div');
-        // div.style.display = 'none';
-        debug.id = "debug";
-        debug.innerHTML = debugviewr;
-        document.body.appendChild(debug);
-    }
-
-
-    var div = document.createElement('div');
-    div.id = "controller";
-    div.innerHTML = controller;
-
-    let messagewrapper = document.createElement('div');
-    messagewrapper.id = 'messages';
-    messagewrapper.innerHTML = messageimgs;
-
-
-    let volumeButton = document.createElement('div');
-    volumeButton.classList.add('volumeButton');
-    volumeButton.innerHTML =volumedom;
-
-
-
-    document.body.appendChild(div);
-    document.body.appendChild(volumeButton);
-
-
-
-    document.getElementById('showcase').appendChild(messagewrapper);
-
-
-    const gui = new GUI();
-
-
-    const audio = new AudioManager(gui);
-    const manager = new SceneManager('main',gui);
-    const interactivebox = new InteractiveBox();
-    const domStyles = new ControllerStyle(audio);
-    const scene_particle = new ScenePartcles(manager,audio, domStyles);
-    manager.addScene(scene_particle);
-    // manager.addScene(interactivebox);
-    manager.update();
 }
-
-$(document).on("MainStart", MainStart);
-// const MainStart = ()=> {
-//
-//
-//     webgl();
-// };
-
+document.addEventListener("DOMContentLoaded", (event)=> {
+    MainStart();
+});
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const poststylus = require('poststylus');
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+//node_modules\.bin\webpack-dev-server --port 3000 --hot --host 0.0.0.0
+// .\node_modules\.bin\webpack
+module.exports = {
+
+    entry: './src/js/main.js',
+    output: {
+
+        path: `${__dirname}/docs`,
+        // 出力ファイル名
+        filename: 'assets/js/jintai_webgl.js'
+    },
+
+    plugins: [
+
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/pug/index.pug',  // この行を変更
+            inject: true
+        }),
+
+    ],
+    module: {
+        rules: [
+            {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: 'awesome-typescript-loader'
+            },
+            // ソースマップファイルの処理
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            },
+            {
+                test: /\.pug$/,
+                loader: ['raw-loader', 'pug-html-loader']
+            },
+            {
+                // Stylusファイル用の処理
+                test: /\.(css|sass|styl)/,
+                use: ExtractTextPlugin.extract({
+                    use: ["css-loader", "stylus-loader"]
+                })
+            },
+            {
+                test: /\.(jpg|png|gif|svg|)$/,
+                loader: ['url-loader']
+            },
+            {
+                test: /\.(glsl|vs|fs|frag|vert)$/,
+                loader: 'shader-loader'
+            },
+            {
+                test: /\.less$/,
+                loader: ['style-loader', 'css-loader', 'less-loader'],
+                exclude: /node_modules/
+            },
+
+            {
+                // 拡張子 .js の場合
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ["transform-class-properties"],
+
+                    }
+                }
+
+            },
+        ]
+    },
+    // import 文で .ts ファイルを解決するため
+    resolve: {
+        extensions: [
+            '.ts', '.js', '.json', '.pug', '.styl', '.glsl', '.frag', '.vert'
+        ],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'docs'),
+        port: 3000,
+        host: "0.0.0.0"
+    },
+    // ソースマップを有効に
+    // devtool: 'source-map'
+};
